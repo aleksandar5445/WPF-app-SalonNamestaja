c2e203d87abcaa65ed14fd49f8af886bf0ee8fef
aleksandar5445 <dimitrov5445@gmail.com>
2017-11-07 19:35:18 +0100

    GenericSerializer

--- rs12-2011/Program.cs
+++ rs12-2011/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace rs12_2011
+﻿using rs12_2011.model;
+
+namespace rs12_2011
 {
     class Program
     {
@@ -7,6 +9,20 @@
         {
             var admin = new Administracija();
             admin.Start();
+
+            var tp1 = new Namestaj()
+            {
+                Sifra = "1",
+                Naziv = "Podna Lampa",
+
+            };
+            var n1 = new Namestaj()
+            {
+                Sifra = "2",
+                Naziv = "Socijalno osvetljenje",
+                TipNamestaja = "Predsoblje",
+            };
+            Util.GenericSerializer.Serialize<Namestaj>("namestaj.xml");
         }
     }
 }
---
+++ rs12-2011/Util/GenericSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+namespace rs12_2011.Util
+{
+    public static class GenericSerializer
+    {
+        public static void Serialize<T>(string path, List<T> listToSerialize)
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                using (var sw = new StreamWriter(path))
+                {
+                    serializer.Serialize(sw, listToSerialize);
+                }
+            }
+            catch
+            {
+            }
+
+        }
+        private static List<T> Deserialize<T>(string outputPath)
+        {
+            var ser = new XmlSerializer(typeof(T));
+            using (var reader = XmlReader.Create(outputPath))
+            {
+                return (List<T>)ser.Deserialize(reader);
+            }
+        }
+
+    }
+}
